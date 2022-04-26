2f00f554e933583be69b803cead3fb75c6972918
Matt Straathof <matt.straat@gmail.com>
2022-04-26 15:10:54 -0700

    fix: publish lambda layer scripts

--- build-scripts/build-lambda-layer.sh
+++ build-scripts/build-lambda-layer.sh
@@ -9,9 +9,12 @@ if [[ $CURRENT_DIR -ne 'build-scripts' ]]; then
 fi
 
 pushd ..
-  mkdir -p nodejs/node_modules
-  pushd nodejs/node_modules
-    npm i @gomomento/sdk
-  popd
-  zip -r layer.zip nodejs
+  TEST="0.8.3"
+  bar=${TEST//\./_}
+  echo $bar
+#  mkdir -p nodejs/node_modules
+#  pushd nodejs/node_modules
+#    npm i @gomomento/sdk
+#  popd
+#  zip -r layer.zip nodejs
 popd
