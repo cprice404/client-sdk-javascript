d10b0782943c39d8c4bf5d2147e0ad50e7816b50
Chris Price <chris@momentohq.com>
2023-06-23 11:27:45 -0700

    WIP: cleanups

--- examples/web/vite-docker/chromium/src/launch-chromium.ts
+++ examples/web/vite-docker/chromium/src/launch-chromium.ts
@@ -1,4 +1,3 @@
-// import playwright = require('playwright-aws-lambda');
 import * as playwright from '@playwright/test';
 
 async function launchChromium(): Promise<void> {
@@ -7,8 +6,12 @@ async function launchChromium(): Promise<void> {
   const browser = await playwright.chromium.launch({headless: true});
   // TODO: make configurable
   const numBrowsers = 10;
-  // TODO: this only works in the docker compose setup, needs to use service discovery to work in ECS
-  const url = 'http://webserver.topics.momento';
+  // TODO: for now we have to choose the URL based on whether we are running locally in
+  // docker-compose, or running in ECS.
+  // Uncomment this line for docker compose environment:
+  const url = 'http://webserver';
+  // OR, Uncomment this line for ECS environment:
+  //const url = 'http://webserver.topics.momento';
   const browserContexts = await Promise.all(
     range(numBrowsers).map(() => browser.newContext())
   );
@@ -28,36 +31,8 @@ async function launchChromium(): Promise<void> {
   console.log('Closing browser');
   await browser.close();
 
-  //
-  // const browser = await playwright.launchChromium();
-  // await browser.newContext();
-
   console.log('done');
-
-  //
-  // let browser;
-  // try {
-  //   browser = await playwright.launchChromium();
-  //   //
-  //   // const context = await browser.newContext();
-  //   // const page = await context.newPage();
-  //   // await page.goto(env.consoleUrl);
-  //   //
-  //   // const loginInput = page.getByLabel('Email address');
-  //   // await loginInput.fill('github-integration@botmail.momentohq.com');
-  //   //
-  //   // await page.getByRole('button', {name: 'Continue', exact: true}).click();
-  //   //
-  //   // console.log('Login attempt initiated; Continue button clicked.');
-  //   //
-  //   // console.log(
-  //   //   `Waiting up to 5 minutes for a message on SQS queue: ${validationQueueUrl}`
-  //   // );
 }
-//
-// function delay(ms: number) {
-//   return new Promise(resolve => setTimeout(resolve, ms));
-// }
 
 function range(count: number): Array<number> {
   return [...Array(count).keys()];
