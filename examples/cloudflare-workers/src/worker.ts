f6c60fc00919ea6f149cfc8e44216731ad1107ed
Chris Price <chris@momentohq.com>
2023-08-03 12:55:04 -0700

    WIP: working example

--- examples/cloudflare-workers/src/worker.ts
+++ examples/cloudflare-workers/src/worker.ts
@@ -7,91 +7,156 @@
  *
  * Learn more at https://developers.cloudflare.com/workers/
  */
-import {CacheClient, Configurations, CredentialProvider} from "@gomomento/sdk-web";
+import {
+	AuthClient,
+	CacheClient,
+	CacheGet,
+	Configurations,
+	CredentialProvider,
+	ExpiresIn,
+	GenerateAuthToken, ListCaches, TokenScopes
+} from "@gomomento/sdk-web";
+import XMLHttpRequestPolyfill from "xhr4sw";
+import {InternalSuperUserPermissions} from "@gomomento/sdk-core/dist/src/internal/utils";
 
-class MomentoFetcher {
-	private readonly apiToken: string;
-	private readonly baseurl: string;
-	constructor(token: string, endpoint: string) {
-		this.apiToken = token;
-		this.baseurl = `${endpoint}/cache`;
-	}
-
-	async get(cacheName: string, key: string) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}`);
-		if (resp.status < 300) {
-			console.log(`successfully retrieved ${key} from cache`)
-		} else {
-			throw new Error(`failed to retrieve item from cache: ${cacheName}`)
-		}
-
-		return await resp.text();
-	}
-
-	async set(cacheName: string, key: string, value: string, ttl_seconds: number = 30) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}&&ttl_seconds=${ttl_seconds}`, {
-			method: 'PUT',
-			body: value
-		});
-
-		if (resp.status < 300) {
-			console.log(`successfully set ${key} into cache`);
-		} else {
-			throw new Error(`failed to set item into cache message: ${resp.statusText} status: ${resp.status} cache: ${cacheName}`);
-		}
-
-		return;
-	}
-
-	async delete(cacheName: string, key: string) {
-		const resp = await fetch(`${this.baseurl}/${cacheName}?key=${key}&token=${this.apiToken}`, {
-			method: 'DELETE',
-		});
-		if (resp.status < 300) {
-			console.log(`successfully deleted ${key} from cache`);
-		} else {
-			throw new Error(`failed to delete ${key} from cache. Message: ${resp.statusText}; Status: ${resp.status} cache: ${cacheName}`);
-		}
-
-		return resp;
-	}
-}
+Object.defineProperty(self, 'XMLHttpRequest', {
+	configurable: false,
+	enumerable: true,
+	writable: false,
+	value: XMLHttpRequestPolyfill
+});
 
 export interface Env {
-	MOMENTO_AUTH_TOKEN: string;
+	MOMENTO_SESSION_TOKEN: string;
 	MOMENTO_REST_ENDPOINT: string;
 	MOMENTO_CACHE_NAME: string;
 }
 
+async function verifyWorkerApiToken(workerApiToken: string, httpEndpoint: string, cacheName: string) {
+	const testKey = 'testkey';
+	const testValue = 'testvalue';
+
+	// set a value in the cache:
+	const setResp = await fetch(
+		`https://${httpEndpoint}/cache/${cacheName}?key=${testKey}&token=${workerApiToken}&ttl_seconds=10`,
+		{
+			method: 'PUT',
+			body: testValue,
+		}
+	);
+
+	if (setResp.status < 300) {
+		console.log(`successfully set ${testKey} into cache`);
+	} else {
+		throw new Error(
+			`failed to set item into cache message: ${setResp.statusText} status: ${setResp.status} cache: ${cacheName}`
+		);
+	}
+
+	const getResp = await fetch(`https://${httpEndpoint}/cache/${cacheName}?key=${testKey}&token=${workerApiToken}`);
+	if (getResp.status < 300) {
+		console.log(`successfully retrieved ${testKey} from cache`);
+	} else {
+		throw new Error(`failed to retrieve item from cache: ${cacheName}`);
+	}
+
+	const value = await getResp.text();
+	if (value === testValue) {
+		console.log('Retrieved expected value from cache!');
+	} else {
+		throw new Error(`Retrieved unexpected value '${value}' from cache!`);
+	}
+}
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const webSdkClient = new CacheClient(
-			{
-				configuration: Configurations.Browser.v1(),
-				credentialProvider: CredentialProvider.fromEnvironmentVariable({environmentVariableName: 'MOMENTO_AUTH_TOKEN'}),
-				defaultTtlSeconds: 60
-			}
-		);
-		console.log(`Instantiated web sdk client`);
-
-
-		const client = new MomentoFetcher(env.MOMENTO_AUTH_TOKEN, env.MOMENTO_REST_ENDPOINT);
 		const cache = env.MOMENTO_CACHE_NAME;
-		const key = "key";
-		const value = "value";
+		// The session token will come from auth0
+		const sessionToken = env.MOMENTO_SESSION_TOKEN;
+		if (sessionToken === undefined) {
+			throw new Error('Missing required env var MOMENTO_SESSION_TOKEN');
+		}
 
-		// setting a value into cache
-		const setResp = await client.set(cache, key, value);
-		console.log("setResp", setResp);
+// These DNS names will be based on which region the user selects
+		const baseEndpoint = 'cell-us-east-1-1.prod.a.momentohq.com';
+		const controlEndpoint = `control.${baseEndpoint}`;
+		const cacheEndpoint = `cache.${baseEndpoint}`;
+		const httpEndpoint = `api.cache.${baseEndpoint}`;
 
-		// getting a value from cache
-		const getResp = await client.get(cache, key)
-		console.log("getResp", getResp);
+// Here we construct an auth client, which is where the token APIs are exposed
+		const authClient = new AuthClient({
+			credentialProvider: CredentialProvider.fromString({
+				authToken: sessionToken,
+				controlEndpoint: controlEndpoint,
+				cacheEndpoint: cacheEndpoint,
+			}),
+		});
 
-		const deleteResp = await client.delete(cache, key);
-		console.log("deleteResp", deleteResp);
+// Here we generate a temporary superuser token that can be used to list caches in the region
+		const wizardTokenResponse = await authClient.generateAuthToken(
+			new InternalSuperUserPermissions(),
+			ExpiresIn.seconds(30)
+		);
+		let wizardApiToken;
+		if (wizardTokenResponse instanceof GenerateAuthToken.Success) {
+			wizardApiToken = wizardTokenResponse.authToken;
+		} else {
+			throw new Error(`A problem occurred when generating auth token: ${wizardTokenResponse.toString()}`);
+		}
 
-		// deleting a value from cache
-		return new Response(JSON.stringify({ response: getResp }));
-	},
-};
+// Now we create a cache client for interacting with the cache APIs
+		const cacheClient = new CacheClient({
+			configuration: Configurations.Browser.v1(),
+			credentialProvider: CredentialProvider.fromString({authToken: wizardApiToken}),
+			defaultTtlSeconds: 60,
+		});
+
+// Get the list of existing caches
+		const listCachesResponse = await cacheClient.listCaches();
+		let caches;
+		if (listCachesResponse instanceof ListCaches.Success) {
+			caches = listCachesResponse.getCaches();
+		} else {
+			throw new Error(`A problem occurred when listing caches: ${listCachesResponse.toString()}`);
+		}
+
+		console.log(`Found the following caches:\n${caches.map(c => c.getName()).join('\n')}`);
+
+// arbitrarily selecting the first cache:
+		const chosenCache = caches[0].getName();
+
+// Now we create a more restricted token that can be used in the user's cloudflare worker environment
+		const workerTokenResponse = await authClient.generateAuthToken(
+			TokenScopes.cacheReadWrite(chosenCache),
+			ExpiresIn.never()
+		);
+
+		let workerApiToken;
+		if (workerTokenResponse instanceof GenerateAuthToken.Success) {
+			workerApiToken = workerTokenResponse.authToken;
+		} else {
+			throw new Error(`A problem occurred when generating auth token: ${workerTokenResponse.toString()}`);
+		}
+
+// This is just a sanity check that the new token works properly
+		await verifyWorkerApiToken(workerApiToken, httpEndpoint, chosenCache);
+
+// And now we're all done, and just need to store these values as secrets/env vars!
+		console.log(`Integration complete! Here are the three required env vars:
+
+MOMENTO_AUTH_TOKEN=${workerApiToken}
+MOMENTO_HTTP_ENDPOINT=${httpEndpoint}
+MOMENTO_CACHE=${chosenCache}
+
+  `);
+
+		return new Response(JSON.stringify({response: 'SUCCESS'}));
+
+	}
+}
+
+
+
+
+
+
