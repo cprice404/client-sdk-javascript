998926cadf2c07e589e396277dae7ae5e625379d
Chris Price <chris@momentohq.com>
2024-05-03 16:29:50 -0700

    chore: clean up middleware logging a bit

--- examples/nodejs/ioredis-logging-example/logging-middleware.ts
+++ examples/nodejs/ioredis-logging-example/logging-middleware.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-argument */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
 import {Middleware, MiddlewareRequestHandler} from '@gomomento/sdk';
 import {
   MiddlewareMessage,
@@ -5,22 +8,27 @@ import {
   MiddlewareRequestHandlerContext,
   MiddlewareStatus,
 } from '@gomomento/sdk/dist/src/config/middleware/middleware';
-import {TextDecoder, TextEncoder} from "node:util";
+import {TextDecoder} from 'node:util';
 
 export class LoggingMiddlewareRequestHandler implements MiddlewareRequestHandler {
   private readonly TEXT_DECODER = new TextDecoder();
 
   onRequestBody(request: MiddlewareMessage): Promise<MiddlewareMessage> {
-    console.log(`REQUEST CONSTRUCTOR: ${request._grpcMessage.constructor.name}`);
-    console.log(`REQEUST: ${JSON.stringify(request)}`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-argument
-    console.log(`dictionary name: ${this.TEXT_DECODER.decode(request._grpcMessage.dictionary_name)}`);
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access,@typescript-eslint/no-unsafe-call
-    console.log(`dictionary items: ${JSON.stringify(request._grpcMessage.items.map(i => [this.TEXT_DECODER.decode(i.field), this.TEXT_DECODER.decode(i.value)]))}`);
+    switch (request._grpcMessage.constructor.name) {
+      case '_DictionarySetRequest':
+        console.log(
+          `Setting fields in dictionary '${this.TEXT_DECODER.decode(
+            request._grpcMessage.dictionary_name
+          )}': ${JSON.stringify(
+            request._grpcMessage.items.map(i => [this.TEXT_DECODER.decode(i.field), this.TEXT_DECODER.decode(i.value)])
+          )}`
+        );
+        break;
+      case '_DictionaryFetchRequest':
+        console.log(`Fetching dictionary '${this.TEXT_DECODER.decode(request._grpcMessage.dictionary_name)}'`);
+        break;
+    }
+
     return Promise.resolve(request);
   }
 
@@ -29,6 +37,19 @@ export class LoggingMiddlewareRequestHandler implements MiddlewareRequestHandler
   }
 
   onResponseBody(response: MiddlewareMessage | null): Promise<MiddlewareMessage | null> {
+    switch (response?._grpcMessage.constructor.name) {
+      case '_DictionaryFetchResponse':
+        console.log(
+          `Got fields from dictionary: ${JSON.stringify(
+            response._grpcMessage.found.items.map(i => [
+              this.TEXT_DECODER.decode(i.field),
+              this.TEXT_DECODER.decode(i.value),
+            ])
+          )}`
+        );
+
+        break;
+    }
     return Promise.resolve(response);
   }
 
